a325f4ec44bc4528a5fba27a3ebcc741bcdcdbb9
rakshithm63 <143328214+rakshithm63@users.noreply.github.com>
2024-01-19 21:59:17 +0530

    Time: 113 ms (80.09%), Space: 12.9 MB (100.00%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        index = 0
+
+        # Place non-zero elements at the start of the list
+        for num in nums:
+            if num != 0:
+                nums[index] = num
+                index += 1
+
+        # Fill the remaining positions with zeroes
+        while index < len(nums):
+            nums[index] = 0
+            index += 1
+        
