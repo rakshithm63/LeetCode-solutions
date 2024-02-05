d20fb35d808810ca6d1a202033cee2ac08620fdb
rakshithm63 <143328214+rakshithm63@users.noreply.github.com>
2024-02-05 20:13:28 +0530

    Time: 37 ms (56.87%), Space: 16.7 MB (55.32%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        x = s.split()
+        dic = {}
+        for i in x :
+            dic[i[-1]] = i[:-1]
+        return ' '.join([dic[j] for j in sorted(dic)])
